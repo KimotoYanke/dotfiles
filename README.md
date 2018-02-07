66b91cbf451809294b1122a16e1f192a27491dd3
KimotoYanke <kimotoyanke@gmail.com>
2018-02-07 21:31:46 +0900

    Add install.fish

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dotfiles
+## Dependencies
+- fish
+- git
---
+++ install.fish
@@ -0,0 +1,12 @@
+#!/bin/fish
+
+DOTFILES_PATH = ~/dotfiles
+
+git clone --recursive "$GITHUB_URL" "DOTFILES_PATH"
+
+cd $DOTFILES_PATH
+if [ $PWD = (realpath $DOTFILES_PATH) ] 
+  ./setup.fish
+else
+  echo "[NOT FOUND] $DOTFILES_PATH"
+end
